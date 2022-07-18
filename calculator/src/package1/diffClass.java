0ec7a3311335aa088a042608a322a49e49daa1b5
K-V-Sagar <sgr.venkat24@gmail.com>
2022-07-18 20:13:22 +0530

    adding typeCasting,accessModifier and Calculator ==> 18/7/22

---
+++ calculator/src/package1/diffClass.java
@@ -0,0 +1,13 @@
+package package1;
+
+public class diffClass {
+	public int diff(int a, int b){
+        return (a-b);
+    }
+  
+      // Overloaded diff(). This takes two double parameters
+//    public double diff(double a, double b){
+//        return (a-b);
+//    }
+
+}
---
+++ calculator/src/package1/productClass.java
@@ -0,0 +1,13 @@
+package package1;
+
+public class productClass {
+	
+	public static int pro(int a, int b){
+		return (a*b);
+	}
+
+//    // Overloaded pro(). This takes two double parameters
+//    public double pro(double a, double b){
+//        return (a*b);
+//    }
+}
