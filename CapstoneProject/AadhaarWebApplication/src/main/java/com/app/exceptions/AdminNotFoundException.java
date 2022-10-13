e2fd5292ef0d4735af3a15895473c765cde684ed
K-V-Sagar <sgr.venkat24@gmail.com>
2022-10-13 19:19:51 +0530

    chnages

--- CapstoneProject/AadhaarWebApplication/src/main/java/com/app/exceptions/AdminNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.app.exceptions;
-
-public class AdminNotFoundException extends RuntimeException {
-	public AdminNotFoundException() {
-		super("Invalid admin name or password");
-	}
-}
--- CapstoneProject/AadhaarWebApplication/src/main/java/com/app/exceptions/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.app.exceptions;
-
-public class UserNotFoundException extends RuntimeException {
-	public UserNotFoundException() {
-		super("Invalid username or password");
-	}
-}
